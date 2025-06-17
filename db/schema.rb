4a230a4b8705d20c89a87dc9c9dc337f495f3c30
Darwin Silva <darwins.silva.ds@gmail.com>
2025-06-17 17:46:53 -0300

    Configure lint with rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,8 +12,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 20_250_617_203_234) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
+  create_table 'transactions', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.string 'from_currency'
+    t.string 'to_currency'
+    t.float 'from_value'
+    t.float 'to_value'
+    t.float 'rate'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_transactions_on_user_id'
+  end
+
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
+
+  add_foreign_key 'transactions', 'users'
 end
--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :transaction do
     user { nil }
-    from_currency { "MyString" }
-    to_currency { "MyString" }
+    from_currency { 'MyString' }
+    to_currency { 'MyString' }
     from_value { 1.5 }
     to_value { 1.5 }
     rate { 1.5 }
